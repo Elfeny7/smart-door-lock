2e2cf34bd78dfc419ffd9425ce0f9a4fc53783e3
elfeny7 <ikmalfaris50@gmail.com>
2024-03-31 23:28:24 +0700

    feat: change table and add dummy model

--- src/app/components/door-user-table.tsx
+++ src/app/components/door-user-table.tsx
@@ -4,14 +4,13 @@ import { Table, TableBody, TableCell, TableHead, TableHeadCell, TableRow } from 
 export default function DoorUserTableComponent() {
   return (
     <div className="overflow-x-auto">
-      <Table striped>
+      <Table hoverable>
         <TableHead>
-          <TableHeadCell>Product name</TableHeadCell>
-          <TableHeadCell>Color</TableHeadCell>
-          <TableHeadCell>Category</TableHeadCell>
-          <TableHeadCell>Price</TableHeadCell>
+          <TableHeadCell>User Name</TableHeadCell>
+          <TableHeadCell>Status</TableHeadCell>
+          <TableHeadCell>Email</TableHeadCell>
           <TableHeadCell>
-            <span className="sr-only">Edit</span>
+            <span className="sr-only">Hapus</span>
           </TableHeadCell>
         </TableHead>
         <TableBody className="divide-y">
@@ -21,10 +20,9 @@ export default function DoorUserTableComponent() {
             </TableCell>
             <TableCell>Sliver</TableCell>
             <TableCell>Laptop</TableCell>
-            <TableCell>$2999</TableCell>
             <TableCell>
-              <a href="#" className="font-medium text-cyan-600 hover:underline dark:text-cyan-500">
-                Edit
+              <a href="#" className="font-medium text-red-600 hover:underline dark:text-red-500">
+                Hapus
               </a>
             </TableCell>
           </TableRow>
@@ -34,10 +32,9 @@ export default function DoorUserTableComponent() {
             </TableCell>
             <TableCell>White</TableCell>
             <TableCell>Laptop PC</TableCell>
-            <TableCell>$1999</TableCell>
             <TableCell>
-              <a href="#" className="font-medium text-cyan-600 hover:underline dark:text-cyan-500">
-                Edit
+              <a href="#" className="font-medium text-red-600 hover:underline dark:text-red-500">
+                Hapus
               </a>
             </TableCell>
           </TableRow>
@@ -45,34 +42,9 @@ export default function DoorUserTableComponent() {
             <TableCell className="whitespace-nowrap font-medium text-gray-900 dark:text-white">Magic Mouse 2</TableCell>
             <TableCell>Black</TableCell>
             <TableCell>Accessories</TableCell>
-            <TableCell>$99</TableCell>
             <TableCell>
-              <a href="#" className="font-medium text-cyan-600 hover:underline dark:text-cyan-500">
-                Edit
-              </a>
-            </TableCell>
-          </TableRow>
-          <TableRow className="bg-white dark:border-gray-700 dark:bg-gray-800">
-            <TableCell className="whitespace-nowrap font-medium text-gray-900 dark:text-white">
-              Google Pixel Phone
-            </TableCell>
-            <TableCell>Gray</TableCell>
-            <TableCell>Phone</TableCell>
-            <TableCell>$799</TableCell>
-            <TableCell>
-              <a href="#" className="font-medium text-cyan-600 hover:underline dark:text-cyan-500">
-                Edit
-              </a>
-            </TableCell>
-          </TableRow>
-          <TableRow className="bg-white dark:border-gray-700 dark:bg-gray-800">
-            <TableCell className="whitespace-nowrap font-medium text-gray-900 dark:text-white">Apple Watch 5</TableCell>
-            <TableCell>Red</TableCell>
-            <TableCell>Wearables</TableCell>
-            <TableCell>$999</TableCell>
-            <TableCell>
-              <a href="#" className="font-medium text-cyan-600 hover:underline dark:text-cyan-500">
-                Edit
+              <a href="#" className="font-medium text-red-600 hover:underline dark:text-red-500">
+                Hapus
               </a>
             </TableCell>
           </TableRow>
--- src/app/doors/[doorId]/page.tsx
+++ src/app/doors/[doorId]/page.tsx
@@ -42,7 +42,7 @@ export default function DoorsDetail() {
                         </div>
                     </div>
                     <div className="flex-1 bg-white rounded-lg min-h-fit px-12 py-12 h-fit">
-                        <div className="flex flex-col gap-8">
+                        <div className="flex flex-col gap-6">
                             <h1 className="font-bold text-2xl">Users</h1>
                             <div className="flex flex-row gap-4">
                                 <ButtonComponent text="New User" color="greenFill" />
