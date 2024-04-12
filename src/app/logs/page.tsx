884b76bd5592e4390ce9882ffb550dc911ff27f8
elfeny7 <ikmalfaris50@gmail.com>
2024-04-12 23:26:46 +0700

    feat: edit logs and dashboard

--- src/app/logs/page.tsx
+++ src/app/logs/page.tsx
@@ -4,8 +4,11 @@ export default function Logs() {
     return (
         <>
             <div className="flex flex-row">
-                <div className="h-screen">
+                <div className="h-screen ">
                     <SidebarComponent activePage="logs" />
+                </div>
+                <div className="p-8">
+                    <h1 className="font-bold text-3xl my-4">Log Activity</h1>
                 </div>
             </div>
         </>
