0a480c03cce4f1d017d103fb37fea00d86e9d92d
elfeny7 <ikmalfaris50@gmail.com>
2024-04-24 08:44:33 +0700

    feat: fix user tsx same as model

--- src/components/data/users.tsx
+++ src/components/data/users.tsx
@@ -2,15 +2,15 @@ import { User } from "../model/user";
 
 export class UserListArray {
     userArray: User[] = [
-       new User(101, "ikmal", "mahasiswa", "ikmal@gmail.com"),
-       new User(102, "faris", "mahasiswa", "faris@gmail.com"),
-       new User(103, "musyaffa", "mahasiswa", "musyaffa@gmail.com"),
-       new User(104, "andi", "mahasiswa", "andi@gmail.com"),
-       new User(105, "budi", "dosen", "budi@gmail.com"),
-       new User(106, "jono", "mahasiswa", "jono@gmail.com"),
-       new User(107, "adi", "mahasiswa", "adi@gmail.com"),
-       new User(108, "putri", "mahasiswa", "putri@gmail.com"),
-       new User(109, "reza", "mahasiswa", "reza@gmail.com"),
-       new User(110, "tono", "mahasiswa", "tono@gmail.com"),
+       new User(101, "ikmal", "087382173", "mahasiswa", "ikmal@gmail.com"),
+       new User(102, "faris", "087382173","mahasiswa", "faris@gmail.com"),
+       new User(103, "musyaffa", "087382173","mahasiswa", "musyaffa@gmail.com"),
+       new User(104, "andi", "087382173","mahasiswa", "andi@gmail.com"),
+       new User(105, "budi", "087382173","dosen", "budi@gmail.com"),
+       new User(106, "jono", "087382173","mahasiswa", "jono@gmail.com"),
+       new User(107, "adi", "087382173","mahasiswa", "adi@gmail.com"),
+       new User(108, "putri", "087382173","mahasiswa", "putri@gmail.com"),
+       new User(109, "reza", "087382173", "mahasiswa", "reza@gmail.com"),
+       new User(110, "tono", "087382173","mahasiswa", "tono@gmail.com"),
     ]
 }
